97073013d37d9a1d9154f9bccf8b1a34be6b3e33
Bozhidar (Bunny) <misterbbunny@gmail.com>
2023-01-16 17:10:32 +0200

    selecting all buttons

---
+++ main.js
@@ -0,0 +1,7 @@
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const clearButton = document.querySelectorAll('[data-equals]');
+const deleteButton = document.querySelectorAll('[data-delete]');
+const equalsButton = document.querySelectorAll('[data-all-clear]');
+const previousOperandTextElement = document.querySelectorAll('[data-precious-operand]');
+const currentOperandTextElement = document.querySelectorAll('[data-current-operand]');
